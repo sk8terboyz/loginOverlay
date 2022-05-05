b8085398c0fab8d5d43320ee82e8c523624f1dee
Shane Kennedy <kennedy.shane@att.net>
2022-05-05 12:18:46 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+$("#btn").click(function () {
+	"use strict";
+	$("#ovrly").fadeIn();
+	$("#login").show();
+	$("#login").animate({top: "100"});
+});
+$("#ovrly").click(function () {
+	"use strict";
+	$("#ovrly").hide();
+	$("#login").hide();
+	$("#login").css("top", "-310px");
+});
+$("#cross").click(function () {
+	"use strict";
+	$("#ovrly").hide();
+	$("#login").hide();
+	$("#login").css("top", "-310px");
+});
+$("#submit").click(function() {
+    "use strict";
+    const username = $(".username")[0].value;
+    const password = $(".password")[0].value;
+    if(username == "AA" && password == "a") {
+        console.log('successful login');
+    } else {
+        console.log('username or password incorrect');
+    }
+})
